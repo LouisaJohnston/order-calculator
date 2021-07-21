12405fa8c732c75a6247184b903a8062ae690f28
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-07-20 18:03:59 -0600

    fetch order and tax endpoints

--- solution.js
+++ solution.js
@@ -1,17 +1,25 @@
 const fetch = require("node-fetch");
 
-const getOrders = () => {
-    try {
-        fetch('https://code-challenge-i2hz6ik37a-uc.a.run.app/orders')
-        .then((response) => response.json())
-        .then((data) => {
-            const orders = Object.values(data)
-            console.log(orders)
-        })
+const findSubtotal = orderItems => {
+    let newTotal = 0
+    for (i = 0; i < orderItems.length; i++) {
+        newTotal += orderItems[i].price
     }
-    catch (err) {
-        console.log(err)
-    }
+    return newTotal
 }
 
-getOrders()
+async function fetchOrder() {
+    const orderResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/sfg34`)
+    const orderJson = await orderResponse.json()
+    const taxResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/cities/${orderJson.zip_code}`)
+    const taxJson = await taxResponse.json()
+    let orderName = orderJson.id 
+    let orderCustomer = orderJson.shipping_name
+    let orderItems = orderJson.order_items
+    let orderSubtotal = findSubtotal(orderItems)
+    let orderTax = taxJson.tax_rate
+    
+    console.log(orderTax)
+}
+
+fetchOrder()
