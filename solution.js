483ebba9ea78289a39afb8912d8fce07fc4a668f
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-07-20 18:23:42 -0600

    MVP

--- solution.js
+++ solution.js
@@ -3,13 +3,26 @@ const fetch = require("node-fetch");
 const findSubtotal = orderItems => {
     let newTotal = 0
     for (i = 0; i < orderItems.length; i++) {
-        newTotal += orderItems[i].price
+        let newPrice = orderItems[i].price * .01
+        newTotal += newPrice.toFixed(2) * orderItems[i].quantity
     }
-    return newTotal
+    return newTotal.toFixed(2)
+}
+
+const findTotal = (orderItems, orderTax) => {
+    let taxableTotal = 0
+    for (i = 0; i < orderItems.length; i++) {
+        let newPrice = orderItems[i].price * .01
+        if (orderItems[i].taxable === 1) {
+            taxableTotal += newPrice.toFixed(2) * orderItems[i].quantity
+        }
+    }
+    const newTotal = (taxableTotal * orderTax) + taxableTotal 
+    return newTotal.toFixed(2)
 }
 
 async function fetchOrder() {
-    const orderResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/sfg34`)
+    const orderResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/sfg47`)
     const orderJson = await orderResponse.json()
     const taxResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/cities/${orderJson.zip_code}`)
     const taxJson = await taxResponse.json()
@@ -17,9 +30,10 @@ async function fetchOrder() {
     let orderCustomer = orderJson.shipping_name
     let orderItems = orderJson.order_items
     let orderSubtotal = findSubtotal(orderItems)
-    let orderTax = taxJson.tax_rate
+    let orderTax = taxJson.tax_rate * .01
+    let orderTotal = findTotal(orderItems, orderTax)
     
-    console.log(orderTax)
+    console.log(orderTotal)
 }
 
 fetchOrder()
