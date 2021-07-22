77c12c8ff76a4027da15a4bf0fd383c78d5ca79c
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-07-22 12:53:24 -0600

    More cleanup

--- solution.js
+++ solution.js
@@ -24,14 +24,14 @@ async function fetchOrder() {
     const orderJson = await orderResponse.json();
     const taxResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/cities/${orderJson.zip_code}`);
     const taxJson = await taxResponse.json();
-    let orderName = orderJson.id; 
-    let orderCustomer = orderJson.shipping_name;
-    let orderItems = orderJson.order_items;
-    let orderTax = taxJson.tax_rate * .01;
-    let orderTaxes = (orderTax * 10).toFixed(2);
+    const orderName = orderJson.id; 
+    const orderCustomer = orderJson.shipping_name;
+    const orderItems = orderJson.order_items;
+    const orderTax = taxJson.tax_rate * .01;
+    const orderTaxes = (orderTax * 10).toFixed(2);
     findTotals(orderItems, orderTax);
-    let orderSubtotal = finalSubtotal;
-    let orderTotal = finalTotal;
+    const orderSubtotal = finalSubtotal;
+    const orderTotal = finalTotal;
     console.log('\n', 'Order:', orderName, '\n', 
                 'Customer Name:', orderCustomer, '\n',
                 'Subtotal:', orderSubtotal, '\n',
