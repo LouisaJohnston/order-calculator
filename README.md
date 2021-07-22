46b886780ce3630c2ca9f2c8d3cb81affac2099f
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-07-22 12:44:12 -0600

    Update to CLI

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# order-calculator
+# Order-Calculator
+
+A JavaScript project using CLI arguments to fetch the order ID, customer name, order subtotal, taxes related to the customer shipping address, and order total for any order in this third-party API: https://code-challenge-i2hz6ik37a-uc.a.run.app/docs
+
+To run, enter the following into your terminal with the placeholder text updated to reflect the desired order number: 
+```
+node solution.js PLACEHOLDER
+```
+
--- solution.js
+++ solution.js
@@ -3,9 +3,6 @@ const fetch = require("node-fetch");
 let finalSubtotal = 0;
 let finalTotal = 0;
 
-let inputBox = document.querySelector("#inputbox");
-let orderForm = document.querySelector("#orderform")
-
 const findTotals = (orderItems, orderTax) => {
     let subtotal = 0;
     let taxableTotal = 0;
@@ -22,10 +19,8 @@ const findTotals = (orderItems, orderTax) => {
     finalTotal = newTotal.toFixed(2);
 }
 
-async function fetchOrder(e) {
-    e.preventDefault();
-    console.log(inputBox.value)
-    const orderResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/sfg47`);
+async function fetchOrder() {
+    const orderResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/${process.argv[2]}`);
     const orderJson = await orderResponse.json();
     const taxResponse = await fetch(`https://code-challenge-i2hz6ik37a-uc.a.run.app/cities/${orderJson.zip_code}`);
     const taxJson = await taxResponse.json();
@@ -33,11 +28,15 @@ async function fetchOrder(e) {
     let orderCustomer = orderJson.shipping_name;
     let orderItems = orderJson.order_items;
     let orderTax = taxJson.tax_rate * .01;
+    let orderTaxes = (orderTax * 10).toFixed(2);
     findTotals(orderItems, orderTax);
     let orderSubtotal = finalSubtotal;
     let orderTotal = finalTotal;
-    console.log(orderSubtotal)
-    console.log(orderTotal)
+    console.log('\n', 'Order:', orderName, '\n', 
+                'Customer Name:', orderCustomer, '\n',
+                'Subtotal:', orderSubtotal, '\n',
+                'Taxes:', orderTaxes, '\n',
+                'Total:', orderTotal)
 }
 
-orderForm.addEventListener("submit", fetchOrder);
+fetchOrder()
