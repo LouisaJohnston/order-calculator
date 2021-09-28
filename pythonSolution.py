989e22a384412abc574bd37eb029531f7f9fa8a4
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-09-28 14:53:34 -0700

    begin python cli

--- pythonSolution.py
+++ pythonSolution.py
@@ -1,8 +1,9 @@
 import requests
 
 def get_order():
-    response = requests.get("https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/sfg47")
+    order_id = input('Please enter an order id:\n')
+    response = requests.get(f'https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/{order_id}')
     response_json = response.json()
-    print(response_json)
+    print(response_json['shipping_name'])
 
 get_order()
