1cf5d899835be1162389fb4a9b5c5158a63e08dd
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-09-28 15:00:12 -0700

    get tax response

--- pythonSolution.py
+++ pythonSolution.py
@@ -2,8 +2,10 @@ import requests
 
 def get_order():
     order_id = input('Please enter an order id:\n')
-    response = requests.get(f'https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/{order_id}')
-    response_json = response.json()
-    print(response_json['shipping_name'])
+    order_response = requests.get(f'https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/{order_id}')
+    order_json = order_response.json()
+    order_zip = order_json['zip_code']
+    tax_response = requests.get(f'https://code-challenge-i2hz6ik37a-uc.a.run.app/cities/{order_zip}')
+    tax_json = tax_response.json()
 
 get_order()
